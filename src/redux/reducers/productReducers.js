b940adc379b9702449360dedef0f3a3fd8fe31c9
rice-warlord <barrettbarrettbarrett@protonmail.com>
2023-12-12 21:05:40 -0500

    Finished ProductDetails.js

---
+++ src/redux/reducers/productReducers.js
@@ -0,0 +1,32 @@
+import { ActionTypes } from "../constants/actionTypes";
+
+const initialState = {
+    products: [],
+}
+
+export const productReducer = (state = initialState, {type,payload}) => {
+    switch (type) {
+        case ActionTypes.SET_PRODUCTS:
+            return {
+                ...state,
+                ...payload,
+            };
+
+    
+        default:
+            return state;
+    }
+};
+
+export const selectedProductReducer = (state={}, {type,payload}) => {
+    switch (type) {
+        case ActionTypes.SELECTED_PRODUCT:
+            return {
+                ...state,
+                products : payload,
+            }
+
+        default:
+            return state;
+    }
+}
